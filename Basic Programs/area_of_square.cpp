2930c0e0b05ca40d8609dfd18f5dd6bd7c4d2788
Abaid Ali Miran <aabaid737@gmail.com>
2023-09-06 11:21:45 +0500

    Find area of square.

---
+++ Basic Programs/area_of_square.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+	int height,width;
+	cout<<"Enter height of square:";
+	cin>>height;
+	cout<<"Enter width of square:";
+	cin>>width;
+	int area=height*width;
+	cout<<"Area of square is :"<<area;
+
+return 0;
+}
+
+
