b5a8cce526e6ab3cb95b83ad8b3530aa4d01b2ef
Abaid Ali Miran <aabaid737@gmail.com>
2023-10-31 07:54:55 +0500

    Snake, Water, Gun game
    This is a simple Snake, Water, Gun game. The computer makes a random choice, and you pick one too. The code then decides if you win or lose. You can make it more fun by adding extra things to the game.

--- Mini Projects/game.cpp
+++ Mini Projects/game.cpp
@@ -2,59 +2,74 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-bool gameWin(char comp, char you) {
-	if (comp == 's') {
-        if (you == 'w') {
-            return false;
-        } else if (you == 'g') {
-            return true;
-        }
-    } else if (comp == 'w') {
-        if (you == 'g') {
-            return false;
-        } else if (you == 's') {
-            return true;
-        }
-    } else if (comp == 'g') {
-        if (you == 's') {
-            return false;
-        } else if (you == 'w') {
-            return true;
-        }
-   }
+class RockPaperScissors{
+	private:
+		char comp, you;
+		int result;
+	public:
+		RockPaperScissors(){
+			srand(time(0));
+    		int randNo = rand() % 3 + 1;
+
+    		if (randNo == 1) {
+        		comp = 's';
+    		} else if (randNo == 2) {
+        		comp = 'w';
+    		} else if (randNo == 3) {
+        		comp = 'g';
+    		}
+		}
+		void user_choice(){
+			cout << "Your Turn: Snake(s), Water(w) or Gun(g)?";
+    		cin >> you;
+		}
+		bool gameWin(char comp, char you) {
+			if (comp == 's') {
+        		if (you == 'w') {
+            		return false;
+        		} else if (you == 'g') {
+            		return true;
+        		}
+    		} else if (comp == 'w') {
+        		if (you == 'g') {
+            		return false;
+        		} else if (you == 's') {
+            		return true;
+        		}
+    		} else if (comp == 'g') {
+        		if (you == 's') {
+            		return false;
+        		} else if (you == 'w') {
+            		return true;
+        	}
+  		}
+    }
+     void printResult() {
+     	result = gameWin(comp,you);
+        if (you == 's' || you == 'w' || you == 'g') {
+        	cout << "Computer Choose: " << comp << endl;
+        	cout << "You choose: " << you << endl;
+        	if (comp == you){
+    			cout << "The Game is Tie!" << endl;
+			}else if (result) {
+       	  		cout << "You Win!" << endl;
+        	} else if (result == false) {
+            	cout << "You Lose!" << endl;
+       		} 
+        } else {
+        	cout << "Wrong Choice!" << endl;
+    }
 }
 
+		
+};
+
+
 int main() {
-    char comp, you;
-    srand(time(0));
-    int randNo = rand() % 3 + 1;
-
-    if (randNo == 1) {
-        comp = 's';
-    } else if (randNo == 2) {
-        comp = 'w';
-    } else if (randNo == 3) {
-        comp = 'g';
-    }
-
+	RockPaperScissors rps;
     cout << "Comp Turn: Snake(s), Water(w) or Gun(g)?" << endl;
-    cout << "Your Turn: Snake(s), Water(w) or Gun(g)?";
-    cin >> you;
-    if (you == 's' || you == 'w' || you == 'g') {
-       	bool result = gameWin(comp, you);
-        cout << "Computer Choose: " << comp << endl;
-        cout << "You choose: " << you << endl;
-        if (comp == you){
-    	cout << "The Game is Tie!" << endl;
-		}else if (result) {
-       	  	cout << "You Win!" << endl;
-        } else if (result == false) {
-            cout << "You Lose!" << endl;
-       	} 
-    } else {
-        cout << "Wrong Choice!" << endl;
-    }
-
+    rps.user_choice();
+    rps.printResult();
     return 0;
 }
 
